e141c084aef122acb003d7679a884d10c287f76e
ojijo123 <oumaerick173@gmail.com>
2024-09-07 01:35:11 -0700

    the database file

--- database/faskdb.py
+++ database/faskdb.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = '360742d0cf318e57da1f481460b96636'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
